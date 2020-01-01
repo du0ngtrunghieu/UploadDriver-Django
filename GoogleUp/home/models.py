7a35b988ca078bd5bc2e5163dda033d2a133aca5
du0ngtrunghieu <dauxanhrauma000@gmail.com>
2020-01-01 11:12:38 +0700

    fads

--- GoogleUp/home/models.py
+++ GoogleUp/home/models.py
@@ -7,5 +7,5 @@ class Map(models.Model):
     
     id = models.AutoField( primary_key=True)
     map_name = models.CharField(max_length=200)
-    map_data = models.ImageField(upload_to='maps/', storage=gd_storage)
-    video = models.FileField(upload_to='videos/',storage=gd_storage,null=True)
+    map_data = models.ImageField(upload_to='maps/', storage=gd_storage,blank=True, null=True)
+    video = models.FileField(upload_to='videos/',storage=gd_storage,blank=True, null=True)
