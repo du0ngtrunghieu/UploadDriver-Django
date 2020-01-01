13c857835740643b87b2843a5fca8c39701193e3
du0ngtrunghieu <dauxanhrauma000@gmail.com>
2020-01-01 10:20:01 +0700

    a

--- GoogleUp/home/models.py
+++ GoogleUp/home/models.py
@@ -8,4 +8,4 @@ class Map(models.Model):
     id = models.AutoField( primary_key=True)
     map_name = models.CharField(max_length=200)
     map_data = models.ImageField(upload_to='maps/', storage=gd_storage)
-   
+    video = models.FileField(upload_to='videos/',storage=gd_storage,null=True)
--- GoogleUp/home/templatetags/drivelink.py
+++ GoogleUp/home/templatetags/drivelink.py
@@ -4,4 +4,4 @@ register = template.Library()
 
 @register.filter(name="convert_drivelink")
 def convert_drivelink(url):
-    return "http://drive.google.com/uc?export=view&id="+url.split("/")[5]
+    return url.split("/")[5]
